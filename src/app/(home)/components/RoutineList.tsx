305c9a143f9b51ae2cf123bc1116530e6c3ba8ac
seunghwan <sun1238799@naver.com>
2024-08-14 11:55:34 +0900

    design: 홈 페이지 내 루틴 정보가 없을 경우 퍼블리싱

--- src/app/(home)/components/RoutineList.tsx
+++ src/app/(home)/components/RoutineList.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const RoutineList = () => {
   return (
     <div>
-      {RoutineData.map((data) => {
+      {RoutineData?.map((data) => {
         return (
           <div
             className="bg-[#292C33] rounded-[6px] h-[76px] box-border mb-3 mx-5 p-4 text-[#EDEDED] relative"
@@ -21,7 +21,7 @@ const RoutineList = () => {
             })}
             <Image
               src={'./assets/menu.svg'}
-              alt="menu"
+              alt="메뉴"
               width={24}
               height={24}
               className="absolute top-1/2 right-[20px] transform -translate-y-1/2 rotate-90"
@@ -29,13 +29,26 @@ const RoutineList = () => {
           </div>
         );
       })}
+      {RoutineData.length === 0 && (
+        <div className="flex items-center bg-[#292C33] rounded-[6px] h-[76px] box-border mb-3 mx-5 p-4 text-[#EDEDED] relative border-[1px] border-dashed border-[#555555]">
+          <p className="text-[15px] leading-[22px] text-[#999999]">새로운 루틴을 추가해보세요</p>
+
+          <Image
+            src={'./assets/plus.svg'}
+            alt="추가하기"
+            width={14}
+            height={14}
+            className="absolute top-1/2 right-[20px] transform -translate-y-1/2 rotate-90"
+          />
+        </div>
+      )}
     </div>
   );
 };
 
 export default RoutineList;
 
-const RoutineData = [
+const RoutineData: any[] = [
   {
     id: 1,
     title: '무분할 상체 루틴',
