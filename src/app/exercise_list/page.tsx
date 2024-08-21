f61a520dfce60c1801a1ee5f450d48e480d47f08
seunghwan <sun1238799@naver.com>
2024-08-21 14:06:41 +0900

    refactor: 부모 스크롤 안생기게 재수정

--- src/app/exercise_list/page.tsx
+++ src/app/exercise_list/page.tsx
@@ -3,7 +3,10 @@ import ExerciseList from './hydrated-page';
 
 const HydratedExerciseList = () => {
   return (
-    <div className="flex flex-col bg-backgrounds-default text-text-main overflow-hidden">
+    <div
+      className="flex flex-col bg-backgrounds-default text-text-main overflow-hidden"
+      style={{ height: 'calc(100vh - 48px)' }}
+    >
       <ExerciseList />
     </div>
   );
