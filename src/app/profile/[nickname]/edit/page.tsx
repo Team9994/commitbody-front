d7a0a4ff03b8565fea1a1a1f083f735a1636de6f
Hyunwoo Jung <nolang3748@naver.com>
2024-10-27 19:08:21 +0900

    design: 마이페이지 edit 화면 수정

--- src/app/profile/[nickname]/edit/page.tsx
+++ src/app/profile/[nickname]/edit/page.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 
 export default function EditPage() {
   return (
-    <div className="bg-gray-900 text-white p-5 font-sans">
+    <div className="bg-gray-900 text-white p-5 font-sans min-h-screen flex flex-col mb-10">
       <header className="flex items-center justify-between mb-6">
-        <button className="text-2xl">&lt;</button>
+        <button className="text-2xl"></button>
         <h1 className="text-lg font-normal">프로필 수정</h1>
         <span className="bg-red-600 text-white rounded-full w-6 h-6 flex items-center justify-center text-xs"></span>
       </header>
-      <form className="space-y-6">
+      <form className="space-y-6 flex-grow overflow-y-auto">
         <div className="space-y-2">
           <label htmlFor="nickname" className="text-sm text-gray-400">
             닉네임
@@ -30,10 +30,22 @@ export default function EditPage() {
         </div>
         <div className="space-y-2">
           <span className="text-sm text-gray-400">생년월일</span>
-          <div className="flex space-x-3">
-            <input type="number" placeholder="1999" className="flex-1 bg-gray-800 p-3 rounded-md" />
-            <input type="number" placeholder="12" className="flex-1 bg-gray-800 p-3 rounded-md" />
-            <input type="number" placeholder="10" className="flex-1 bg-gray-800 p-3 rounded-md" />
+          <div className="flex space-x-2">
+            <input
+              type="number"
+              placeholder="1999"
+              className="flex-1 bg-gray-800 p-2 rounded-md text-sm w-1/3"
+            />
+            <input
+              type="number"
+              placeholder="12"
+              className=" bg-gray-800 w-1/3 p-2 rounded-md text-sm"
+            />
+            <input
+              type="number"
+              placeholder="10"
+              className=" bg-gray-800 w-1/3 p-2 rounded-md text-sm"
+            />
           </div>
         </div>
         {['키(cm)', '몸무게(kg)', '골격근량(kg)', '체지방률(%)'].map((label, index) => (
