43b0d16f54212a005b33f48ece678081260f8ad0
Hyunwoo Jung <nolang3748@naver.com>
2024-11-23 16:46:07 +0900

    fix: 코드 수정

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,29 +1,3 @@
-import { handlers, updateSession } from '@/auth';
-import { NextResponse } from 'next/server';
+import { handlers } from '@/auth';
 
-export async function POST(request: Request) {
-  try {
-    const { accessToken } = await request.json();
-
-    // 세션 업데이트
-    await updateSession({ accessToken });
-
-    return NextResponse.json({
-      success: true,
-      message: '세션이 업데이트되었습니다.',
-    });
-  } catch (error) {
-    console.error('세션 업데이트 실패:', error);
-    return NextResponse.json(
-      {
-        success: false,
-        message: '세션 업데이트에 실패했습니다.',
-      },
-      {
-        status: 500,
-      }
-    );
-  }
-}
-
-export const { GET } = handlers;
+export const { GET, POST } = handlers;
