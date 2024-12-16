224b659bde1cf530e05b56278cccde580cdb2546
seunghwan <sun1238799@naver.com>
2024-12-17 00:09:58 +0900

    test: CD 테스트

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -29,7 +29,7 @@ export default async function Home() {
             className="flex items-center w-full h-10 text-sm leading-5 py-2.5 rounded-6 bg-[#324151]"
           >
             <Image className="m-2" src={'/assets/search.svg'} width={24} height={24} alt="돋보기" />
-            <span className="text-text-light">혹시 궁금한 운동이 있으신가요?</span>
+            <span className="text-text-light">혹시 궁금한 운동이 있으신가요?!</span>
           </Link>
         </div>
         <h4 className="text-[18px] font-semibold leading-[26px] py-2 pl-5 text-text-main">
