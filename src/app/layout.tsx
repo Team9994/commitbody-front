d99ce0d44c829e5aa0071bd45cc575875634e734
seunghwan <sun1238799@naver.com>
2024-12-28 01:37:32 +0900

    feat: GA 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import AuthSession from '@/lib/AuthSession';
 import ReactQueryProvider from '@/providers/ReactQueryProvider';
+import GoogleAnalytics from '@/lib/GoogleAnalytics';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,6 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en" className="w-full bg-gray-400">
       <body className={`${inter.className} max-w-[500px] m-auto shadow-custom-light z-20`}>
+        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
+          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+        ) : null}
         <ReactQueryProvider>
           <AuthSession>{children}</AuthSession>
           {/* <ReactQueryDevtools /> */}
